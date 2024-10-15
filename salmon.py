34890953c87b8d2389085546060dc4d90bacbd91
Bertrand Chenal <bertrandchenal@gmail.com>
2024-10-15 09:31:40 +0200

    add multi-threaded scheduler

--- salmon.py
+++ salmon.py
@@ -1,3 +1,7 @@
+from queue import SimpleQueue
+import threading
+
+
 Msg = str
 Context = list[Msg]
 
@@ -10,27 +14,63 @@ class Stream:
     def append(self, msg: Msg):
         self.items.append(msg)
 
-    def context(self, size: int = 0) -> Context:
-        return self.items[-size:]
+    @property
+    def context(self):
+        return self.items
 
 
 class Scheduler:
-
     def __init__(self, stream: Stream, *agents: "Agent"):
         self.stream = stream
         self.agents = agents
 
+
+class RoundRobinScheduler(Scheduler):
     def epoch(self) -> Context:
         for ag in self.agents:
-            msg = ag.prompt(self.stream.context(ag.context_len))
+            msg = ag.prompt(self.stream.context)
             if msg is not None:
                 self.stream.append(msg)
-        return self.stream.context()
+        return self.stream.context
+
+
+class ThreadedScheduler(Scheduler):
+    def __init__(self, stream: Stream, *agents: "Agent"):
+        super().__init__(stream, *agents)
+        self.context_q = SimpleQueue()
+        self.result_q = SimpleQueue()
+
+        # Create threads
+        agent_threads = []
+        for ag in self.agents:
+            t = threading.Thread(
+                target=ag.run,
+                daemon=True,
+                args=(self.context_q, self.result_q),
+            )
+            agent_threads.append(t)
+            t.start()
+
+        # Bootstrap agents
+        for _ in self.agents:
+            self.context_q.put(self.stream.context)
+
+    def epoch(self) -> Context:
+        msg = self.result_q.get()
+        self.stream.append(msg)
+        for _ in self.agents:
+            self.context_q.put(self.stream.context)
+        return self.stream.context
 
 
 class Agent:
-    def __init__(self, context_len: int = 1):
-        self.context_len = context_len
-
     def prompt(self, context):
         return None
+
+    def run(self, context_q:SimpleQueue, result_q:SimpleQueue):
+        while True:
+            ctx = context_q.get()
+            print(self, 'got', ctx)
+            msg = self.prompt(ctx)
+            if msg:
+                result_q.put(msg)
--- salmon_test.py
+++ salmon_test.py
@@ -1,5 +1,8 @@
-from time import sleep
-from salmon import Agent, Scheduler, Stream
+'''
+Install `typeguard` and run me with:
+  pytest --typeguard-packages=salmon
+'''
+from salmon import Agent, RoundRobinScheduler, ThreadedScheduler, Stream
 
 
 class TickerAgent(Agent):
@@ -14,13 +17,27 @@ class UpperEchoAgent(Agent):
         return context[-1].upper()
 
 
-def test_ticker():
+def test_rr():
     agents = [
         TickerAgent(),
         UpperEchoAgent(),
     ]
     stream = Stream()
-    scheduler = Scheduler(stream, *agents)
+    scheduler = RoundRobinScheduler(stream, *agents)
     for _ in range(3):
         ctx = scheduler.epoch()
     assert ctx == ['tick', 'TICK', 'tick', 'TICK', 'tick', 'TICK']
+
+
+def test_threaded():
+    agents = [
+        TickerAgent(),
+        UpperEchoAgent(),
+    ]
+    stream = Stream()
+    scheduler = ThreadedScheduler(stream, *agents)
+    for _ in range(3):
+        ctx = scheduler.epoch()
+    # Here we can not test for exact result
+    assert ctx
+    assert all(i in ('tick', 'TICK') for i in ctx)
